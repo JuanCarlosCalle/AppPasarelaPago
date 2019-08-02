e0fe64656b9f4fa501d5d2c6d47b911a1249cf78
JUANCARLOS <juan.ing.udea@gmail.com>
2019-08-01 20:35:24 -0500

    Solucionado error librerias

--- AppPasarelaPago-ejb/target/generated-sources/annotations/com/udea/modelo/Articulo_.java
+++ AppPasarelaPago-ejb/target/generated-sources/annotations/com/udea/modelo/Articulo_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-30T20:49:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-31T10:15:10")
 @StaticMetamodel(Articulo.class)
 public class Articulo_ { 
 
